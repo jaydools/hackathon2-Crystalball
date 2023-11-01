c579025e689abeb2f6aa041eb037946878dfca97
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-01 19:17:23 -0400

    added basic styling
    mixins are not working as expected, however variables are
    
    move back to topLeftContainer and fix styling

--- src/Components/RightContainer/RightContainer.js
+++ src/Components/RightContainer/RightContainer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./RightContainer.scss";
 
 function RightContainer() {
   return (
-    <div>
-        <h2>Explination</h2>
+    <div className='explination'>
+        <h2 className='explination__header'>Explination</h2>
+        <div className='explination__text'>
+          <p>This is a text placeholder
+                {/* INSERT DYNAMICS HERE */}
+              how to use the app will be displayed here</p>
+        </div>
     </div>
   )
 }
