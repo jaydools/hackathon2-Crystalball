ac44450c8f1b7a3a5daa4363ff4a620b4021ff95
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-01 17:49:43 -0400

    first commit on new branch
    add basic layout and style

--- src/Components/LeftContainer/LeftContainer.js
+++ src/Components/LeftContainer/LeftContainer.js
@@ -2,10 +2,14 @@ function LeftContainer() {
 
 
 
-
-
     return (
         <body>
+            <div className="activity">
+                <h1>Choose Activity</h1>
+            </div>
+            <div>
+                <button>Random</button>
+            </div>
 
         </body>
     )
