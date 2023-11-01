7a31c27613a3759ad983c26706aafec5454a959c
alex.drouillard <alex.drouillard.9@hotmail.com>
2023-11-01 18:05:01 -0400

    LeftBottom Container basic layout

--- src/Components/LeftContainerBottom/LeftContainerBottom.js
+++ src/Components/LeftContainerBottom/LeftContainerBottom.js
@@ -1,23 +1,24 @@
+import './LeftContainerBottom.scss';
+
 function LeftContainerBottom() {
-
-
-
-
-
     return (
         <body>
             <div className="container">
-            <div className="container__left">
-                <div className="container__left--top">
+                <div className="container__top">
                     <h3 label for="number">Number of Participants</h3>
                     <input type="text" name="number" id="number" placeholder="  Add number of Participants" className="upload__form--top--box"/>
                 </div>
-                <div className="container__left--bottom">
+                <div className="container__bottom">
+                    <h2>Price Point</h2>
+                    <div>
+                    <h3 label for="number">Free</h3>
+                    <input type="text" name="number" id="number" placeholder="FREE" />
+                    </div>
+                    <div>
+                    <h3 label for="number">Not Free</h3>
+                    <input type="text" name="number" id="number" placeholder="$$$$" />
+                    </div>
                 </div>
-            </div>
-            <div className="container__right">
-                
-            </div>
             </div>
         </body>
     )
