57e688f56839011f88459faf03b4059804d32f5f
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-01 19:49:35 -0400

    finial push for now
    meet with team to discuss styles

--- src/Components/RightContainer/RightContainer.js
+++ src/Components/RightContainer/RightContainer.js
@@ -6,9 +6,9 @@ function RightContainer() {
     <div className='explination'>
         <h2 className='explination__header'>Explination</h2>
         <div className='explination__text'>
-          <p>This is a text placeholder
-                {/* INSERT DYNAMICS HERE */}
-              how to use the app will be displayed here</p>
+          <p>This is a text placeholder</p>
+          <p>We can put something like, If you are bored and need to come up with something to do, you've come to the right spot. </p>
+          <p>maybe we make it more cryptic tho</p>
         </div>
     </div>
   )
