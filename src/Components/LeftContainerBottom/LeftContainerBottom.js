abf02b8a80317e0f87c73ca8963f71e8a0b0af51
Srinidhi Shaw <srinidhishaw@gmail.com>
2023-11-02 13:49:44 -0400

    no changes

--- src/Components/LeftContainerBottom/LeftContainerBottom.js
+++ src/Components/LeftContainerBottom/LeftContainerBottom.js
@@ -1,71 +1,69 @@
 import './LeftContainerBottom.scss';
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function LeftContainerBottom() {
-    const [activity, setActivity] = useState([]); // State to track the selected option
-
-    const handleSubmitRan = (event) => {
-        event.preventDefault();
-        async function RandomAct(){
-            const act = await axios.get(`https://www.boredapi.com/api/activity/`)
-            console.log(act.data)
-            setActivity(act.data)
-        }
-        RandomAct()
+    const [activity, setActivity] = useState({}); // State to track the selected option
+    
+  const handleSubmitRan = (event) => {
+    event.preventDefault();
+    async function RandomAct(){
+        const act = await axios.get(`https://www.boredapi.com/api/activity/`)
+        console.log(act.data)
+        setActivity(act.data)
+      }
+      RandomAct()
   }
-  
-    const handleSubmitPart = (event) => {
-        event.preventDefault();
-        async function PartAct(){
+  const handleSubmitPart = (event) => {
+    event.preventDefault();
+    async function PartAct(){
         const formData = new FormData(event.target);
-        const num = formData.get('number');
-            const act = await axios.get(`https://www.boredapi.com/api/activity?participants=${num}`)
-            setActivity(act.data)
-            console.log(act.data)
-        }
-        PartAct()
+    const num = formData.get('number');
+        const act = await axios.get(`https://www.boredapi.com/api/activity?participants=${num}`)
+        setActivity(act.data)
+      }
+      PartAct()
   }
-  
-    const handleSubmitFree = (event) => {
-        event.preventDefault();
-        async function FreeAct(){
-            const act = await axios.get(`https://www.boredapi.com/api/activity?price=0.0`)
-            setActivity(act.data)
-            console.log(act.data)
-        }
-        FreeAct()
+  const handleSubmitFree = (event) => {
+    event.preventDefault();
+    async function FreeAct(){
+        const act = await axios.get(`https://www.boredapi.com/api/activity?price=0.0`)
+        setActivity(act.data)
+      }
+      FreeAct()
   }
-
-
     return (
+        <body>
             <div className="container">
             <form onSubmit={handleSubmitRan}>
             <button type="submit">Random</button>
             </form>
-
             <form onSubmit={handleSubmitPart}>
                 <div className="container__top">
-                    <div className="container__card">
-                        <h3 label for="number">Number of Participants</h3>
-                        <input type="text" name="number" id="number" placeholder=" Add number of Participants" className="submitBox"/>
-                        <button type="submit" className='button1'>Submit</button>
-                    </div>
+                    <h3 label for="number">Number of Participants</h3>
+                    <input type="text" name="number" id="number" placeholder="  Add number of Participants" className="upload__form--top--box"/>
+                    <button type="submit">Submit</button>
                 </div>
                 </form>
+                
                 <div className="container__bottom">
-                    <div className="container__card2">
-                        <h2>Price Point</h2>
-                        <form onSubmit={handleSubmitFree}>
-                        <button type="submit" className='button2'>Free</button>
-                        </form>
-                        <form onSubmit={handleSubmitFree}>
-                        <button type="submit" className='button3'>Not Free</button>
-                        </form>
-                 </div>
+                    <h2>Price Point</h2>
+                    <form onSubmit={handleSubmitFree}>
+                    <div>
+            <button type="submit">Free</button>
+          </div>
+          </form>
+          <form onSubmit={handleSubmitFree}>
+          <div>
+            <button type="submit">Not Free</button>
+          </div>
+          </form>
+          </div>
+          
+        
+        
             </div>
-            </div>
+        </body>
     )
 }
 
